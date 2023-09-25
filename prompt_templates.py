0b941ff5ab0c2dfc755e010af32b23922e547ed0
Daniel Medrano <daniel_medrano@Daniel-Medrano-Hakkoda-FVFH2034Q05R.local>
2023-09-25 09:49:02 -0600

    Added simple chatbot.

---
+++ prompt_templates.py
@@ -0,0 +1,5 @@
+GENERATE_FORMATTED_ANSWER = """
+Format the plain answer below in first person. Please respond politely and attentively, addressing the user's query with clear and helpful information. Don't use fancy words. Only include the formatted answer in your response.
+
+{plain_answer}
+"""
